fa8d3ea858164d831a9087081fb57d252de25291
Glaze96 <rasmusramen@gmail.com>
2022-10-25 19:13:51 +0200

    basic landing page done, scroll to art function done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,41 @@
+import React, { useRef } from "react";
 import { NextPage } from "next";
 import Image from "next/image";
 import Layout from "../components/Layout/Layout";
 import Button from "../components/Misc/button";
 
+const scrollToRef = (ref : any) => window.scrollTo(0, ref.current.offsetTop);
+
 const Home: NextPage = () => {
+
+  const myRef = useRef(null);
+  const executeScroll = () => scrollToRef(myRef);
+
   return (
-    <div className="bg-black">
-      <main>
+    <div>
+      <main className="bg-black">
         <Layout>
-          <div className="flex py-20">
-            <img
-              className="m-auto w-1/2"
-              src="/Logo/RECT LOGO_svg.svg"
-              alt="logo"
-            />
-          </div>
-          <div className="flex m-auto">
-            <div className="grid grid-rows-4 gap-4">
-              <h2 className="text-xl">CHECK OUT MY...</h2>
-              <Button label="MUSIC" />
-              <Button label="ART" />
-              <Button label="ME!" />
+          <div className="h-screen bg-black">
+            <div className="flex py-20">
+              <img
+                className="m-auto w-screen md:w-1/2"
+                src="/Logo/RECT LOGO_svg.svg"
+                alt="logo"
+              />
             </div>
+            <div className="flex m-auto">
+              <div className="flex m-auto text-lg gap-3 flex-col md:flex-row">
+                <h2 className="m-auto">CHECK OUT: </h2>
+                <Button label="MY ART" callback={() => executeScroll()}/>
+                <Button label="MY MUSIC" />
+                <Button label="ME!" />
+              </div>
+            </div>
+          </div>
+        </Layout>
+        <Layout>
+          <div ref={myRef} className="h-screen bg-secondary">
+            <h2 className="text-lg">MY ART</h2>
           </div>
         </Layout>
       </main>
