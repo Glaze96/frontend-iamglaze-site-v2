db2ebceb381c5e1e005abb01860daa9c2df6dc34
Glaze96 <rasmusramen@gmail.com>
2022-10-25 15:52:24 +0200

    added logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import { NextPage } from "next";
-import Image from "next/image"
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div>
       <main>
-
-        <Image src="/Logo/RECT LOGO_svg.svg"></Image>
-
+        <div className="grid grid-cols-2">
+          <h1 className="text-2xl">HELLO THERE</h1>
+          <div className="place-content-center h-screen">
+            <img src="/Logo/RECT LOGO_svg.svg" alt="logo" />
+          </div>
+        </div>
       </main>
 
       <footer></footer>
